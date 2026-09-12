408629936a5bcd3b3f67c6b546c5b6654e2698ed
prajuhadapad06-maker <prajuhadapad06@gmail.com>
2026-09-12 03:27:01 -0300

    Update vote.py

--- vote.py
+++ vote.py
@@ -1,5 +1,6 @@
 age=input("enter your age:")
 if age>=18:
-  printf("you are eligible for voting")
+  print("you are eligible for voting")
+  
 else:
-  printf("you are not eligible for voting")
+  print("you are not eligible for voting")
